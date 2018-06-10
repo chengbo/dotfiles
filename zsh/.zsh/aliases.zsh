fd1981ea34167016078fb862e99ce1b060e21d96
Bo Cheng <chengbo1983@gmail.com>
2018-06-10 10:55:12 -0700

    fix ls alias not found in linux

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -1,2 +1,9 @@
-alias ls='/usr/local/bin/gls --color -h --group-directories-first'
+case `uname` in
+  Darwin)
+    alias ls='/usr/local/bin/gls --color -h --group-directories-first'
+  ;;
+  Linux)
+    alias ls='/bin/ls --color=tty --group-directories-first'
+  ;;
+esac
 alias http='http --print=hb'

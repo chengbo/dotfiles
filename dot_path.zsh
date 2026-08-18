a8a41f5d074a1d765b60e223af8727958dbab315
Bo Cheng <chengbo1983@gmail.com>
2026-08-17 18:38:54 -0700

    MCP config symlinking, codex/claude tweaks

--- dot_path.zsh
+++ dot_path.zsh
@@ -7,6 +7,8 @@ export PATH="$HOME/bin:$PATH"
 export PATH="$HOME/.cargo/bin:$PATH"
 export PATH="$PATH:/snap/bin"
 
+export PATH="$PATH:/opt/nvim-linux-x86_64/bin"
+
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
